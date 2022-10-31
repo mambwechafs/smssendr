e418535c73921a243d90aad080bde89438ea3f93
Mambwe Chafungwa <115482550+Whitechafs101@users.noreply.github.com>
2022-10-31 16:34:50 +0200

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,13 +1,14 @@
+#pip install twilio
 from twilio.rest import Client 
  
-account_sid = 'AC88ec45fc543afc69a91560d3921af980' 
-auth_token = 'ece145543eab8667bee532d60b8666d9'
+account_sid = '' #enter account sid
+auth_token = ' ' #enter auth token
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create(  
-                              messaging_service_sid='MG2568d4c4d29ac728b0eedee2d7dfdb19', 
-                              body='I cant believe this works',      
-                              to='+260963777748' 
-                          ) 
+                              messaging_service_sid='', 
+                              body=' ',      
+                              to='   ' 
+                          ) #enter messaging service id, the message and the phone number you are sending to
  
 print(message.sid)
